a430b747616c09c6808d26d780dbd5df873674d9
jianghao <jianghao2>
2017-09-19 20:24:31 +0800

    增加获取开奖类型的接口

--- Lucifer-common/src/main/java/com/bc/lottery/entity/LotteryType.java
+++ Lucifer-common/src/main/java/com/bc/lottery/entity/LotteryType.java
@@ -1,6 +1,8 @@
 package com.bc.lottery.entity;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by luis on 2017/4/13.
@@ -8,4 +10,16 @@ import java.io.Serializable;
 
 public interface LotteryType <E extends Enum<E>>{
 
+    static LotteryType parseType(int lotteryId, long playId) {
+        switch (lotteryId) {
+            case 1:
+                return ShishicaiType.parse(playId);
+            default:
+                return ShishicaiType.parse(playId);
+        }
+    }
+
+    long value();
+
+    String desc();
 }
