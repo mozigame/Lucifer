fde4456ab304be39dd7f0790dc2bc3831992a304
joey <jianghao2>
2017-11-28 15:26:59 +0800

    修改pom引用

--- Lucifer-common/src/main/java/com/bc/lottery/entity/LotteryType.java
+++ Lucifer-common/src/main/java/com/bc/lottery/entity/LotteryType.java
@@ -7,27 +7,27 @@ package com.bc.lottery.entity;
 public interface LotteryType<E extends Enum<E>> {
 
     static LotteryType parseType(long lotteryId, long playId) {
-
-        switch (String.valueOf(lotteryId)) {
-            case 1 + "":
+        int lotteryChange = (int) lotteryId;
+        switch (lotteryChange) {
+            case 1:
                 return ShishicaiType.parse(playId);
-            case 2 + "":
-            case 12 + "":
-            case 14 + "":
+            case 2:
+            case 12:
+            case 14:
                 return ShishicaiDoubleType.parse(playId);
-            case 3 + "":
+            case 3:
                 return Lottery11x5Type.parse(playId);
-            case 4 + "":
-            case 16 + "":
-            case 18 + "":
+            case 4:
+            case 16:
+            case 18:
                 return Lottery11x5DoubleType.parse(playId);
-            case 6 + "":
-            case 20 + "":
-            case 22 + "":
+            case 6:
+            case 20:
+            case 22:
                 return LotteryKuai3DoubleType.parse(playId);
-            case 7 + "":
+            case 7:
                 return LotteryPK10Type.parse(playId);
-            case 8 + "":
+            case 8:
                 return LotteryPK10DoubleType.parse(playId);
             default:
                 return ShishicaiType.parse(playId);
